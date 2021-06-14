97b2c2f7b80ae1c7aea1a1ab7619efd27c6dc370
peppelinux <giuseppe.demarco@unical.it>
2021-06-14 19:26:56 +0200

    fix: demo_run static httpd now is in https

--- demo-run.sh
+++ demo-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SATOSA_APP=/usr/lib/python3.8/site-packages/satosa
-uwsgi --uid 1000 --http 0.0.0.0:9999 --check-static-docroot --check-static $BASEDIR/static/ --static-index disco.html &
+uwsgi --uid 1000 --https 0.0.0.0:9999,$BASEDIR/pki/cert.pem,$BASEDIR/pki/privkey.pem --check-static-docroot --check-static $BASEDIR/static/ --static-index disco.html &
 P1=$!
 uwsgi --uid 1000 --wsgi-file $SATOSA_APP/wsgi.py  --https 0.0.0.0:10000,$BASEDIR/pki/cert.pem,$BASEDIR/pki/privkey.pem --callable app
 P2=$!
