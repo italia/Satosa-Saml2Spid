aca34ebe13ae70c1aa648405b259924b43970ba3
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2022-05-09 10:49:56 +0200

    added "SATOSA_" prefix for proxy_conf.yaml

--- example/bash_replace.sh
+++ example/bash_replace.sh
@@ -4,11 +4,11 @@ if [[ -v SAML2_REQUESTED_ATTRIBUTES ]]; then
   sed -i "s/required_attributes: \[\]/required_attributes: \[$SAML2_REQUESTED_ATTRIBUTES\]/" plugins/backends/saml2_backend.yaml
 fi
 
-if [[ -v BACKEND_MODULES ]]; then
-  sed -i "s/BACKEND_MODULES: \[\]/BACKEND_MODULES: \[$BACKEND_MODULES\]/" proxy_conf.yaml
+if [[ -v SATOSA_BACKEND_MODULES ]]; then
+  sed -i "s/BACKEND_MODULES: \[\]/BACKEND_MODULES: \[$SATOSA_BACKEND_MODULES\]/" proxy_conf.yaml
 fi
 
-if [[ -v FRONTEND_MODULES ]]; then
-  sed -i "s/FRONTEND_MODULES: \[\]/FRONTEND_MODULES: \[$FRONTEND_MODULES\]/" proxy_conf.yaml
+if [[ -v SATOSA_FRONTEND_MODULES ]]; then
+  sed -i "s/FRONTEND_MODULES: \[\]/FRONTEND_MODULES: \[$SATOSA_FRONTEND_MODULES\]/" proxy_conf.yaml
 fi
 
