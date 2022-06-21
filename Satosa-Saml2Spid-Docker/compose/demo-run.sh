f4508ad5aed881c17b7ef914733e9ec214f7f8ce
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2022-06-21 14:20:24 +0200

    docker su http (nginx farà da proxy)

--- Satosa-Saml2Spid-Docker/compose/demo-run.sh
+++ Satosa-Saml2Spid-Docker/compose/demo-run.sh
@@ -139,9 +139,9 @@ else
 fi
 
 SATOSA_APP=/usr/lib/python3.8/site-packages/satosa
-uwsgi --uid 1000 --https 0.0.0.0:9999,$BASEDIR/pki/cert.pem,$BASEDIR/pki/privkey.pem --check-static-docroot --check-static $BASEDIR/static/ --static-index disco.html &
+uwsgi --uid 1000 --http 0.0.0.0:9999 --check-static-docroot --check-static $BASEDIR/static/ --static-index disco.html &
 P1=$!
-uwsgi --uid 1000 --wsgi-file $SATOSA_APP/wsgi.py  --https 0.0.0.0:10000,$BASEDIR/pki/cert.pem,$BASEDIR/pki/privkey.pem --callable app -b 32648
+uwsgi --uid 1000 --wsgi-file $SATOSA_APP/wsgi.py  --http 0.0.0.0:10000 --callable app -b 32648
 P2=$!
 wait $P1 $P2
 #wait $P2
