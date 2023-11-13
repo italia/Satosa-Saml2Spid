b29f338cbc123b9eb05da74986181436cef1e571
Gartic99 <artic.1804049@studenti.uniroma1.it>
2023-11-13 16:38:31 +0100

    feat: implemented Django template for the example sp

--- example_sp/djangosaml2_sp/djangosaml2_sp/settings.py
+++ example_sp/djangosaml2_sp/djangosaml2_sp/settings.py
@@ -109,6 +109,7 @@ SESSION_COOKIE_AGE = 60 * 60  # an hour
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+DJANGO_BOOTSTRAP_ITALIA_USE_CDN = False
 
 if 'saml2_sp' in INSTALLED_APPS or \
    'djangosaml2_spid' in INSTALLED_APPS:
--- example_sp/djangosaml2_sp/saml2_sp/views.py
+++ example_sp/djangosaml2_sp/saml2_sp/views.py
@@ -26,20 +26,25 @@ from .utils import repr_saml
 
 
 logger = logging.getLogger('djangosaml2')
-
+context = {
+        "LOGOUT_URL" : settings.LOGOUT_URL,
+        "LOGIN_URL" : settings.LOGIN_URL,
+        "LOGIN_REDIRECT_URL" : settings.LOGIN_REDIRECT_URL
+    }
 
 def index(request):
     """ Barebone 'diagnostics' view, print user attributes if logged in + login/logout links.
     """
-    if request.user.is_authenticated:
-        out = "LOGGED IN: <a href={0}>LOGOUT</a><br>".format(settings.LOGOUT_URL)
-        out += "".join(['%s: %s</br>' % (field.name, getattr(request.user, field.name))
-                    for field in request.user._meta.get_fields()
-                    if field.concrete])
-        return HttpResponse(out)
-    else:
-        return HttpResponse("LOGGED OUT: <a href={0}>LOGIN</a>".format(settings.LOGIN_URL))
-
+    context["user"] = request.user
+    return render(request,"base.html",context)
+def amministrazione(request):
+    context["user"] = request.user
+    return render(request,"amministrazione.html",context)
+def echo_attributes(request):
+    context["out"] = "".join(['%s: %s</br>' % (field.name, getattr(request.user, field.name))
+                for field in request.user._meta.get_fields()
+                if field.concrete])
+    return render(request,"echo_attributes.html",context)
 
 # TODO fix this in IdP side?
 @receiver(pre_user_save, sender=User)
