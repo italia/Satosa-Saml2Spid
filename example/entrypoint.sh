78f7edcfbba38bc0fe38aea7e72942f0bc7c60c3
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2024-06-21 11:55:17 +0200

    Update entrypoint.sh
    New address for get the SPID IDP metadata

--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -4,7 +4,7 @@
 # get IDEM MDQ key
 if [[ $GET_IDEM_MDQ_KEY == true ]]; then
   wget https://mdx.idem.garr.it/idem-mdx-service-crt.pem -O $SATOSA_KEYS_FOLDER/idem-mdx-service-crt.pem
-  wget https://registry.spid.gov.it/metadata/idp/spid-entities-idps.xml -O metadata/idp/spid-entities-idps.xml
+  wget https://registry.spid.gov.it/entities-idp -O metadata/idp/spid-entities-idps.xml
   echo "Downloaded IDEM MDQ key"
 fi
 
