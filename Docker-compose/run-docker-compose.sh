3c4c1d661201f5aab1cae5cbdda247a59569d986
MDreW <andrea.ranaldi@gmail.com>
2024-08-23 14:20:16 +0200

    example: docker compose witout static volumes

--- Docker-compose/run-docker-compose.sh
+++ Docker-compose/run-docker-compose.sh
@@ -1,46 +1,65 @@
 #!/bin/bash
 
-function create-volume {
-	if [ ! "$(docker volume ls -q -f name=$1)" ]
-	then
-	    echo -e "Il volume $1 non esiste, lo creo! \n"
-	    docker volume create --name=$1
-	    echo -e "\n"
-	    if [[ ! -z "$2" ]]
-	    then 
-	       echo -e "Ho creato il volume e ci copio i dati da $2 \n"
-	       sudo cp -R $2* `docker volume inspect $1 | jq .[0].Mountpoint | sed 's/"//g'`
-	    fi   
-	else
-	    echo -e "Il volume $1 esiste, non faccio nulla! \n"  
-	fi
+function clean_data {
+  rm -Rf ./mongo/db/*
+  rm -Rf ./satosa/*
+  rm -Rf ./djangosaml2_sp/*
 }
 
-create-volume satosa-saml2spid_nginx_certs nginx/certs/
-create-volume satosa-saml2spid_mongodata 
+function initialize_satosa {
+  mkdir -p ./satosa
+  mkdir -p ./djangosaml2_sp
+  mkdir -p ./mongo/db
 
-echo -e "\n"
+  if [ ! -f ./satosa/proxy_conf.yaml ]; then cp -R ../example/* ./satosa/ ; else echo 'satosa directory is already initialized' ; fi
+  if [ ! -f ./djangosaml2_sp/run.sh ]; then cp -R ../example_sp/djangosaml2_sp/* ./djangosaml2_sp ; else echo 'djangosaml2_sp directory is already initialided' ; fi
+}
 
-echo -e "Provo a scaricare le nuove versioni. \n"
+function start {
+  echo -e "Provo a scaricare le nuove versioni. \n"
+  docker compose -f docker-compose.yml pull
+  echo -e "\n"
+  echo -e "Provo a fare il down della composizione. \n"
+  docker compose -f docker-compose.yml down -v
+  echo -e "\n"
+  echo -e "Tiro su la composizione, in caso, con le nuove versioni delle immagini. \n"
+  docker compose -f docker-compose.yml build django_sp
+  docker compose -f docker-compose.yml up -d --wait --wait-timeout 60
+  echo -e "\n"
+  echo -e "Completato. Per visionare i logs: 'docker-compose -f docker-compose.yml logs -f'"
+  exit 0
+}
 
-docker compose -f docker-compose.yml pull
+function help {
+  echo ""
+  echo "### run-docker-compose.sh ###"
+  echo ""
+  echo "initialize and start Satosa-Saml2Spid"
+  echo ""
+  echo "Option"
+  echo "-f Force clean and reinitialize data for Satosa, MongoDB and Djangosaml2_SP"
+  echo "-h Print this help"
+  echo ""
+}
 
-echo -e "\n"
+while getopts ":fh" opt; do
+  case ${opt} in
+   f)
+     clean_data
+     ;;
+   h)
+     help
+     exit 0
+     ;;
+   ?)
+     echo "Invalid option: -${OPTARG}."
+     echo ""
+     help
+     exit 1
+     ;;
+  esac
+  initialize_satosa
+  start
+done
 
-echo -e "Provo a fare il down della composizione. \n"
 
-docker compose -f docker-compose.yml down -v
-
-echo -e "\n"
-
-echo -e "Tiro su la composizione, in caso, con le nuove versioni delle immagini. \n"
-
-docker compose -f docker-compose.yml build django_sp
-
-docker compose -f docker-compose.yml up -d --wait --wait-timeout 60
-
-echo -e "\n"
-
-echo -e "Completato. Per visionare i logs: 'docker-compose -f docker-compose.yml logs -f'"
-
-exit 0
